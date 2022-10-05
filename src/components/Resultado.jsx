759d2a3e8ac977d99cdffc52174959c531a55447
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-05 14:34:29 -0700

    Formateando el Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,26 +1,60 @@
 import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
 const Resultado = ({ resultado }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     resultado;
   return (
-    <div>
-      <p>
-        El Precio es de: <span>{PRICE}</span>
-      </p>
-      <p>
-        Precio mas alto del dia: <span>{HIGHDAY}</span>
-      </p>
-      <p>
-        Precio mas bajo del dia: <span>{LOWDAY}</span>
-      </p>
-      <p>
-        Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
-        Ultima Actulizacion: <span>{LASTUPDATE}</span>
-      </p>
-    </div>
+    <Contenedor>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen cripto"
+      />
+      <div>
+        <Precio>
+          El Precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio más alto del día: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio más bajo del día: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Última Actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
   );
 };
 
