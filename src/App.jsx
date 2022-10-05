fff1b44a648910d981aa2100f245a59bf4e161f1
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-05 14:45:17 -0700

    Añadiendo un Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ImagenCripto from "./img/imagen-criptos.png";
 import Formulario from "./components/Formulario";
 import { useEffect, useState } from "react";
 import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -44,14 +45,19 @@ const Heading = styled.h1`
 function App() {
   const [monedas, setMonedas] = useState({});
   const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
+        setCargando(true);
+        setResultado({});
         const { moneda, criptomoneda } = monedas;
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
         setResultado(resultado.DISPLAY[criptomoneda][moneda]);
+        setCargando(false);
       };
       cotizarCripto();
     }
@@ -62,6 +68,7 @@ function App() {
       <div>
         <Heading>Cotiza Cripto Monedas Al Instante</Heading>
         <Formulario setMonedas={setMonedas} />
+        {cargando && <Spinner />}
         {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
